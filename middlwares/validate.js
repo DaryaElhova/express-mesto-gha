2dd3c133816d941c60830803768024e77dc40f08
Daria Elkhova <darya.elhova@yandex.ru>
2023-06-16 22:44:26 +0200

    sprint14

--- middlwares/validate.js
+++ middlwares/validate.js
@@ -1,8 +1,10 @@
 const { celebrate, Joi } = require('celebrate');
 
+const regex = /^https?:\/\/(www\.)?[a-zA-Z0-9-._~:\/?#[\]@!$&'()*+,;=]+#?$/;
+
 const validateAuth = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().email({ minDomainSegments: 2 }).required().unique(),
+    email: Joi.string().email({ minDomainSegments: 2 }).required(),
     password: Joi.string().required().min(4),
   }),
 });
@@ -11,8 +13,8 @@ const validateCreateUser = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(2).max(30),
-    avatar: Joi.string(),
-    email: Joi.string().email({ minDomainSegments: 2 }).required().unique(),
+    avatar: Joi.string().pattern(regex),
+    email: Joi.string().email({ minDomainSegments: 2 }).required(),
     password: Joi.string().required().min(4),
   }),
 });
@@ -26,14 +28,14 @@ const validateUpdateUser = celebrate({
 
 const validateUpdateAvatar = celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string(),
+    avatar: Joi.string().pattern(regex),
   }),
 });
 
 const validateCreateCard = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required(),
+    link: Joi.string().required().pattern(regex),
   }),
 });
 
