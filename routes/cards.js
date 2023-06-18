6bfd856a78297b5986d8a44faf0d48b670381ca5
Daria Elkhova <darya.elhova@yandex.ru>
2023-06-18 23:07:23 +0200

    fix

--- routes/cards.js
+++ routes/cards.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const cardsController = require('../controllers/cards');
 const { auth } = require('../middlwares/auth');
-const { validateCreateCard } = require('../middlwares/validate');
+const { validateCreateCard, validateId } = require('../middlwares/validate');
 
 router.use(auth);
 router.get('/cards', cardsController.getCards);
 router.post('/cards', validateCreateCard, cardsController.createCard);
-router.delete('/cards/:_id', cardsController.deleteCard);
-router.put('/cards/:_id/likes', cardsController.likeCard);
-router.delete('/cards/:_id/likes', cardsController.deleteLike);
+router.delete('/cards/:_id', validateId, cardsController.deleteCard);
+router.put('/cards/:_id/likes', validateId, cardsController.likeCard);
+router.delete('/cards/:_id/likes', validateId, cardsController.deleteLike);
 
 module.exports = router;
